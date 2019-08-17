5f7d3c5a1b0fceda89c65cfaf35b2dadcd96f290
Rutger Broekhoff <rutger@viasalix.nl>
2019-08-17 11:29:47 +0200

    Try searching after driver has not been found

--- driver_name.go
+++ driver_name.go
@@ -13,33 +13,41 @@ type sqlDriverNames struct {
 	sync.Mutex
 }
 
+// driverNames stores all mapped drivers by name.
 var driverNames = sqlDriverNames{
 	byType: make(map[reflect.Type]string),
 }
 
 // The database/sql API doesn't provide a way to get the registry name for
 // a driver from the driver type.
-// See https://github.com/golang/go/issues/12600#issuecomment-378363201
+// Adapter from https://github.com/golang/go/issues/12600#issuecomment-378363201
 func sqlDriverToDriverName(driver driver.Driver) string {
 	driverNames.Lock()
 	defer driverNames.Unlock()
 
-	if len(driverNames.byType) == 0 {
-		for _, driverName := range sql.Drivers() {
-			// We ignore this error because it will occur. We only need the
-			// driver connected to the name.
-			db, _ := sql.Open(driverName, "")
+	driverType := reflect.TypeOf(driver)
 
-			if db != nil {
-				driverType := reflect.TypeOf(db.Driver())
-				driverNames.byType[driverType] = driverName
-			}
+	if len(driverNames.byType) > 0 {
+		if driverName, found := driverNames.byType[driverType]; found {
+			return driverName
 		}
 	}
 
-	driverType := reflect.TypeOf(driver)
-	if driverName, found := driverNames.byType[driverType]; found {
-		return driverName
+	for _, driverName := range sql.Drivers() {
+		// We ignore this error because it will occur. We only need the
+		// driver connected to the name.
+		db, _ := sql.Open(driverName, "")
+
+		if db != nil {
+			driverType := reflect.TypeOf(db.Driver())
+			driverNames.byType[driverType] = driverName
+		}
+	}
+
+	if len(driverNames.byType) > 0 {
+		if driverName, found := driverNames.byType[driverType]; found {
+			return driverName
+		}
 	}
 
 	return ""
